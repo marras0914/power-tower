e2b1c2b93d43968b2de847218f018dcf17872b39
Arras <MArras@loanpacific.com>
2016-10-06 16:45:22 -0500

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	
+    	//Navigation
+        // menu: '#menu',
+        lockAnchors: false,
+       	anchors: ['1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9'],
+        // navigation: false,
+        // navigationPosition: 'right',
+        // navigationTooltips: ['firstSlide', 'secondSlide'],
+        // showActiveTooltip: false,
+        // slidesNavigation: true,
+        // slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        // continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: true,
+        interlockedSlides: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6', 'yellow', 'orange', '#C0C0C0', '#ADD8E6', 'red'],
+        // paddingTop: '3em',
+        // paddingBottom: '100px',
+        fixedElements: '#header, footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+});
